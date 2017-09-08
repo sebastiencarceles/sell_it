a116aca0b5f5f8b58a262fbeba93062f58907a60
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-09-08 19:52:44 +0200

    add new route and action to get all the classifieds

--- spec/classifieds_spec.rb
+++ spec/classifieds_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Classifieds API', type: :request do
+  describe 'GET /classifieds' do
+    before { 
+      FactoryGirl.create_list :classified, 3
+      get "/classifieds" 
+    }
+
+    it 'works' do
+      expect(response).to be_success
+    end
+
+    it 'returns all the entries' do
+      expect(parsed_body.count).to eq Classified.all.count
+    end
+  end
+
   describe 'GET /classifieds/:id' do
     let(:classified) { FactoryGirl.create :classified }
 
+    before { get "/classifieds/#{classified.id}" }
+
     it 'works' do
-      get "/classifieds/#{classified.id}"
       expect(response).to be_success
     end
 
     it 'is correctly serialized' do
-      get "/classifieds/#{classified.id}"
       expect(parsed_body['title']).to eq classified.title
       expect(parsed_body['price']).to eq classified.price
       expect(parsed_body['description']).to eq classified.description
