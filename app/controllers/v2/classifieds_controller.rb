a34c74e165ba1a42a50bfea4dda1090e02e18f0f
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-09-20 16:20:42 +0200

    use scopes on Classified model instead of direct queries in the controller

--- app/controllers/v2/classifieds_controller.rb
+++ app/controllers/v2/classifieds_controller.rb
@@ -4,9 +4,9 @@ class V2::ClassifiedsController < V1::ClassifiedsController
       render json: { error: "missing parameter #{param_sym.to_s}" }, status: :bad_request and return unless params[param_sym]
     end
     render json: { error: 'order parameter must be asc or desc' }, status: :bad_request and return unless params[:order] == 'asc' || params[:order] == 'desc'
-    scope = Classified.where(category: params[:category]) if params[:category]
+    scope = Classified.by_category(params[:category]) if params[:category]
     scope ||= Classified.all
-    scope = scope.where('title like ?', "%#{params[:q]}%") if params[:q]
+    scope = scope.by_title_query(params[:q]) if params[:q]
     paginate json: scope.order(created_at: params[:order]), status: :partial_content
   end
 
--- app/models/classified.rb
+++ app/models/classified.rb
@@ -3,4 +3,7 @@ class Classified < ApplicationRecord
 
   validates_presence_of :user, :title, :price, :description
   validates_numericality_of :price
+
+  scope :by_category, ->(category) { where(category: category) }
+  scope :by_title_query, ->(title_query) { where('title like ?', "%#{title_query}%") }
 end
