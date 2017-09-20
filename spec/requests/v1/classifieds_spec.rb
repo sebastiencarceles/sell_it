b8178d855e61eb15f767e330477b90746a60989b
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-09-20 16:16:57 +0200

    better spec organization

--- spec/requests/v1/classifieds_spec.rb
+++ spec/requests/v1/classifieds_spec.rb
@@ -53,11 +53,11 @@ RSpec.describe 'Classifieds API', type: :request do
       get '/v1/classifieds/toto'
       expect(response).to have_http_status :not_found
     end
+  end
 
-    context 'publications' do
-      before { post "/v1/classifieds/#{classified.id}/publications" }
-      it { expect(response).to have_http_status :created }
-    end
+  describe 'POST /classifieds/:id/publications' do
+    before { post "/v1/classifieds/#{classified.id}/publications" }
+    it { expect(response).to have_http_status :created }
   end
 
   describe 'POST /classifieds' do
